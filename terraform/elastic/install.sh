4d76beb227123b734d08a955693736e313e6725f
Jacob Cantwell <jacob.cantwell@arq.group>
2024-06-18 04:30:09 +0000

    Updating terraform install

--- terraform/elastic/install.sh
+++ terraform/elastic/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
 echo "Installing Terraform"
-sudo yum install -y yum-utils shadow-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+curl -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip && mv terraform /usr/bin
+terraform --version
